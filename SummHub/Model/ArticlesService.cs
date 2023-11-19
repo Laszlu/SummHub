b220428ba5e5a748721b25cf72e99622d249fae8
DavidR <david.rid4@gmail.com>
2023-11-19 16:08:45 +0100

    ArticlesService

--- SummHub/Model/ArticlesService.cs
+++ SummHub/Model/ArticlesService.cs
@@ -1,17 +1,26 @@
 /*********************************************************************************************************************/
 
-namespace Model {
-public List<NewsArticle> TopStories { get; set; }
+namespace Model
+{
 
-public List<NewsArticle> Sports { get; set; }
+    public class ArticlesService
+    {
+        public List<NewsArticle> TopStories { get; set; }
 
-public List<NewsArticle> Politics { get; set; }
+        public List<NewsArticle> Sports { get; set; }
 
-public List<NewsArticle> Science { get; set; }
+        public List<NewsArticle> Politics { get; set; }
 
-public List<NewsArticle> Business { get; set; }
+        public List<NewsArticle> Science { get; set; }
 
-public List<NewsArticle> Entertainment { get; set; }
+        public List<NewsArticle> Business { get; set; }
 
-public void SaveArticles (List<NewsArticle> articleList, Category category)
+        public List<NewsArticle> Entertainment { get; set; }
+
+
+        public void SaveArticles(List<NewsArticle> articleList, Category category)
+        {
+
+        }
+    }
 }
