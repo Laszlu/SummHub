f7082764b41b2fcb2e33ba90ee984d43855c39f2
DavidR <david.rid4@gmail.com>
2023-11-19 15:59:12 +0100

    Enum Added

---
+++ SummHub/Model/Category.cs
@@ -0,0 +1,15 @@
+/*********************************************************************************************************************/
+// von David 
+
+namespace Model
+{
+    public enum Category
+    {
+        TopStorys,
+        Sports,
+        Politics,
+        Science,
+        Business,
+        Entertainment
+    }
+}
