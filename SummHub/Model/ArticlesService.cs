4a96ed03c6c580934f05458b76825417d1acc11c
DavidR <david.rid4@gmail.com>
2023-11-19 16:05:45 +0100

    News Articles

--- SummHub/Model/ArticlesService.cs
+++ SummHub/Model/ArticlesService.cs
@@ -1,6 +1,17 @@
 /*********************************************************************************************************************/
 
 namespace Model {
-public List<NewsArticles> TopStoies { get; set; }
+public List<NewsArticle> TopStories { get; set; }
 
+public List<NewsArticle> Sports { get; set; }
+
+public List<NewsArticle> Politics { get; set; }
+
+public List<NewsArticle> Science { get; set; }
+
+public List<NewsArticle> Business { get; set; }
+
+public List<NewsArticle> Entertainment { get; set; }
+
+public void SaveArticles (List<NewsArticle> articleList, Category category)
 }
