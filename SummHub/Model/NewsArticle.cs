5e41a91d659dce9139827b76e48f8f7fb9767c23
Laszlu <maurice.ferreyra@gmail.com>
2023-11-19 15:58:09 +0100

    added NewsArticle.cs

---
+++ SummHub/Model/NewsArticle.cs
@@ -0,0 +1,20 @@
+/*********************************************************************************************************************/
+namespace Model
+{
+    public class NewsArticle
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime Published { get; set; }
+    }
+}
+
+
+
+/*********************************************************************************************************************/
