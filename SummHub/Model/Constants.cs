b268dc6f37c2eb789de2d08831b379e7e9d187d7
Laszlu <maurice.ferreyra@gmail.com>
2023-11-22 19:31:17 +0100

    rebuild apiController and tested api call

--- SummHub/Model/Constants.cs
+++ SummHub/Model/Constants.cs
@@ -7,7 +7,7 @@ public class NewsApiConstants
 {
     public const string BaseUrlNewsApi = "https://newsapi.org/v2/";
     public const string ApiKeyNewsApi = "apiKey=1980d7327a9948d4a97e0fafb45b3405";
-    public const string TopStoriesCategoryNewsApi = "top-headlines";
+    public const string TopStoriesNewsApi = "top-headlines";
 
     public const string SportsCategoryNewsApi = "category=sports";
 
@@ -15,6 +15,10 @@ public class NewsApiConstants
     public const string ScienceCategoryNewsApi = "category=science";
     public const string BusinessCategoryNewsApi = "category=business";
     public const string EntertainmentCategoryNewsApi = "category=entertainment";
+
+    public const string GeneralCategoryNewsApi = "category=general";
+
+    public const string NewsApiDateFormat = "yyyy-MM-dd";
 }
 
 public class TranslatorConstants
