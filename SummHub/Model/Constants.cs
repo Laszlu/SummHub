01c59190e9a016f0ba41febe98f54c1d83e3206b
DavidR <david.rid4@gmail.com>
2023-11-22 19:31:44 +0100

    df

--- SummHub/Model/Constants.cs
+++ SummHub/Model/Constants.cs
@@ -8,7 +8,6 @@ public class NewsApiConstants
     public const string BaseUrlNewsApi = "https://newsapi.org/v2/";
     public const string ApiKeyNewsApi = "apiKey=1980d7327a9948d4a97e0fafb45b3405";
     public const string TopStoriesCategoryNewsApi = "top-headlines";
-
     public const string SportsCategoryNewsApi = "category=sports";
 
     //public const string PoliticsCategoryNewsApi = "";
