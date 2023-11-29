a406570a3f44638e5392d14ea8bd5f8f5c8f2f36
Laszlu <maurice.ferreyra@gmail.com>
2023-11-29 12:59:36 +0100

    updated MsTranslatorApiController.cs

---
+++ SummHub/Controller/MsTranslatorApiController.cs
@@ -0,0 +1,38 @@
+using System.Net.Http.Headers;
+using static Model.TranslatorConstants;
+
+namespace Controller;
+
+public class MsTranslatorApiController
+{
+    public HttpClient HttpClient { get; set; } = new();
+
+    public async Task<string?> Translate(string textToTranslate)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(UriMsTranslatorApi),
+            Headers =
+            {
+                { ApiKeyPropMsTranslator, ApiKeyMsTranslator },
+                { ApiHostPropMsTranslator, ApiHostMsTranslator },
+            },
+            Content = new StringContent($"[{{\"Text\":\"{textToTranslate}\"}}]")
+            {
+                Headers =
+                {
+                    ContentType = new MediaTypeHeaderValue(MediaTypeMsTranslator)
+                }
+            }
+        };
+
+        using var response = await HttpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var body = await response.Content.ReadAsStringAsync();
+        return body;
+    }
+    
+
+
+}
