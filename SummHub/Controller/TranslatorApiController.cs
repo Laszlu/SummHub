f59cb09ee0dfc4fe5299600849d51e344ea41798
Laszlu <maurice.ferreyra@gmail.com>
2023-11-29 12:35:19 +0100

    started on TranslatorApiController.cs

--- SummHub/Controller/TranslatorApiController.cs
+++ SummHub/Controller/TranslatorApiController.cs
@@ -1,6 +1,34 @@
+using System.Net.Http.Headers;
+
 namespace Controller;
 
 public class TranslatorApiController
 {
+    public HttpClient HttpClient { get; set; }
     
+    
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Post,
+    RequestUri = new Uri("https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "af25d28a40msh05323456eb8d745p1f43fejsnc4353b6fc76b" },
+        { "X-RapidAPI-Host", "microsoft-translator-text.p.rapidapi.com" },
+    },
+    Content = new StringContent("[\n    {\n        \"Text\": \"I would really like to drive your car around the block a few times.\"\n    }\n]")
+    {
+        Headers =
+        {
+            ContentType = new MediaTypeHeaderValue("application/json")
+        }
+    }
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
 }
