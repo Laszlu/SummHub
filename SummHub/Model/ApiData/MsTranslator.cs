5020d7b2c236e4a5dc76d10618b6462ce960f6b7
Laszlu <maurice.ferreyra@gmail.com>
2023-11-29 15:36:40 +0100

    added example response

--- SummHub/Model/ApiData/MsTranslator.cs
+++ SummHub/Model/ApiData/MsTranslator.cs
@@ -4,3 +4,14 @@ public class MsTranslator
 {
 
 }
+
+[{
+    "detectedLanguage": {
+        "language": "es",
+        "score": 0.99
+    },
+    "translations": [{
+        "text": "Israel-Hamas War Live: Gaza Hostage Release, Deaths, News and More - CNN",
+        "to": "en"
+    }]
+}]
