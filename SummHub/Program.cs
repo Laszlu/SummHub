99ba6b07a4dde0ebccdec7db5ae15a7df7e89788
Laszlu <maurice.ferreyra@gmail.com>
2023-12-07 11:17:01 +0100

    removed comments

--- SummHub/Program.cs
+++ SummHub/Program.cs
@@ -9,15 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//var client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<LanguageDetector>();
 builder.Services.AddSingleton<ArticlesService>();
 builder.Services.AddSingleton<NewsApiController>();
 builder.Services.AddSingleton<MsTranslatorApiController>();
 builder.Services.AddSingleton<ControllerManager>();
-
-//builder.Services.AddScoped(sp => new ControllerManager(client));
 
 await builder.Build().RunAsync();
